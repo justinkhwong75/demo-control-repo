d761f2853b2c0e333f5bf97694589ce002d8e9f3
justinkhwong75 <96101651+justinkhwong75@users.noreply.github.com>
2022-10-25 17:17:48 +0800

    Update autosign.pp

--- site-modules/profile/manifests/puppet/master/autosign.pp
+++ site-modules/profile/manifests/puppet/master/autosign.pp
@@ -1,7 +1,7 @@
 class profile::puppet::master::autosign {
   ini_setting { 'policy-based autosigning':
     setting => 'autosign',
-    path    => "${confdir}/puppet.conf",
+    path    => $settings::config,
     section => 'master',
     value   => '/usr/local/bin/autosign-validator',
     notify  => Service['pe-puppetserver'],
